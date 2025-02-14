966b080f6d0767561e6f6cd342967c0a028d8343
“MMakonnen” <mikael.makonnen@gmail.com>
2025-02-14 20:48:42 +0100

    extension config file

--- config.py
+++ config.py
@@ -2,7 +2,11 @@ config = {
 
     # Dataset Selection
     # Options: '10', '20', '50' for 10k, 20k, or 50k dataset (top ... passages of precedent)
-    'dataset': '10',              
+    'dataset': '10',       
+    
+    # Data Usage Fraction
+    # Fraction of the complete dataset to use for faster iterations
+    'data_usage_fraction': 0.2,       
 
     # Train-Test-Validation Split
     'train_test_val_split': {
@@ -11,7 +15,6 @@ config = {
         'test': 0.1
     },
 
-    # Data Usage Fraction
-    # Fraction of the complete dataset to use for faster iterations
-    'data_usage_fraction': 0.2
+    # Seed for Reproducibility
+    'seed': 42
 }
