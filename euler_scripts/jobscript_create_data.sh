54fed82aa6a5af03202a7d8338821b742f2f53c5
“MMakonnen” <mikael.makonnen@gmail.com>
2025-02-25 21:14:29 +0100

    shell scripts to run code on cluster

---
+++ euler_scripts/jobscript_create_data.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#SBATCH --output=logs/create_data/slurm-%j.out
+#SBATCH --mem-per-cpu=30G
+#SBATCH --time=00:30:00
+
+module load stack/2024-06 cuda/12.4.1 eth_proxy
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate lpr-ra
+
+# ...
+python src/create_finetuning_dataset.py
---
+++ euler_scripts/jobscript_finetune_model.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --output=logs/finetune_model/slurm-%j.out
+#SBATCH --gpus=1
+#SBATCH --mem-per-cpu=30G
+#SBATCH --gres=gpumem:23G
+#SBATCH --time=00:30:00
+
+module load stack/2024-06 cuda/12.4.1 eth_proxy
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate lpr-ra
+
+# ...
+python src/finetune_model.py
