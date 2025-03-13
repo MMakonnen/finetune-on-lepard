c0aa7afdb1da39162274da677a6e040233e6ce28
“MMakonnen” <mikael.makonnen@gmail.com>
2025-03-13 21:47:40 +0100

    minor fix

--- euler_scripts/jobscript_create_data.sh
+++ euler_scripts/jobscript_create_data.sh
@@ -6,7 +6,7 @@
 
 module load stack/2024-06 cuda/12.4.1 eth_proxy
 source ~/miniconda3/etc/profile.d/conda.sh
-conda activate lpr-ra
+conda activate finetune-lpr-env
 
 # ...
 python src/create_finetuning_dataset.py
