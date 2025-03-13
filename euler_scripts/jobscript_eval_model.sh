15d893d9e3b23b9c301853dbd114ad24cb458b46
“MMakonnen” <mikael.makonnen@gmail.com>
2025-03-13 21:46:07 +0100

    bash scripts for finetuning and eval

--- euler_scripts/jobscript_eval_model.sh
+++ euler_scripts/jobscript_eval_model.sh
@@ -2,13 +2,15 @@
 
 #SBATCH --output=logs/eval_model/slurm-%j.out
 #SBATCH --gpus=1
-#SBATCH --mem-per-cpu=30G
-#SBATCH --gres=gpumem:23G
-#SBATCH --time=00:30:00
+#SBATCH --mem-per-cpu=40G
+#SBATCH --gres=gpumem:38G
+#SBATCH --time=04:00:00
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --mail-user="ENTER_YOUR_MAIL_HERE"
 
 module load stack/2024-06 cuda/12.4.1 eth_proxy
 source ~/miniconda3/etc/profile.d/conda.sh
-conda activate lpr-ra
+conda activate finetune-lpr-env
 
 # ...
 python src/eval_finetuned_model.py
--- euler_scripts/jobscript_finetune_model.sh
+++ euler_scripts/jobscript_finetune_model.sh
@@ -2,13 +2,15 @@
 
 #SBATCH --output=logs/finetune_model/slurm-%j.out
 #SBATCH --gpus=1
-#SBATCH --mem-per-cpu=30G
-#SBATCH --gres=gpumem:23G
-#SBATCH --time=00:30:00
+#SBATCH --mem-per-cpu=80G
+#SBATCH --gres=gpumem:70G
+#SBATCH --time=18:00:00
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --mail-user="ENTER_YOUR_MAIL_HERE"
 
 module load stack/2024-06 cuda/12.4.1 eth_proxy
 source ~/miniconda3/etc/profile.d/conda.sh
-conda activate lpr-ra
+conda activate finetune-lpr-env
 
 # ...
 python src/finetune_model.py
