37a5c239976914cb1056e5a8ae9fa755c41c77a7
“MMakonnen” <mikael.makonnen@gmail.com>
2025-03-31 02:20:21 +0200

    DistilBERT jobscripts for data creation, finetuning and eval

---
+++ euler_scripts/distilbert/jobscript_distilbert_create_data.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --output=logs/distilbert/create_data/slurm-%j.out
+#SBATCH --mem-per-cpu=30G
+#SBATCH --time=00:30:00
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --mail-user="ENTER_YOUR_MAIL_HERE"
+
+module load stack/2024-06 cuda/12.4.1 eth_proxy
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate finetune-lpr-env
+
+# ...
+python src_distilbert/create_distilbert_data.py
---
+++ euler_scripts/distilbert/jobscript_distilbert_eval_model.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH --output=logs/distilbert/eval_model/slurm-%j.out
+#SBATCH --gpus=1
+#SBATCH --mem-per-cpu=80G
+#SBATCH --gres=gpumem:70G
+#SBATCH --time=4:00:00
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --mail-user="ENTER_YOUR_MAIL_HERE"
+
+module load stack/2024-06 cuda/12.4.1 eth_proxy
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate finetune-lpr-env
+
+# ...
+python src_distilbert/eval_distilbert.py
